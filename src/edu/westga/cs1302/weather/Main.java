3e34a602ba2210f6ff8a307d382c29e3f7258984
alex decesare <adecesa1@my.westga.edu>
2020-07-16 19:37:52 -0400

    Fixed checkstyle errors

--- src/edu/westga/cs1302/weather/Main.java
+++ src/edu/westga/cs1302/weather/Main.java
@@ -1,24 +1,66 @@
 package edu.westga.cs1302.weather;
 	
+import java.io.IOException;
+import java.net.URL;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.AnchorPane;
 
+/**
+ * The main class for the weather project
+ * 
+ * @author Alex DeCesare
+ * @version 16-July-2020
+ */
 
 public class Main extends Application {
+	
+	private static final String VIEW_EVENT_PANE_FXML = "../view/EventPane.fxml";
+	private static final String THE_TITLE = "Alex DeCesare's Project Four";
+	private static final String THE_IO_EXCEPTION_TEXT = "Problem opening the file";
+	private static final String THE_NUMBER_FORMAT_EXCEPTION_TEXT = "Cannot find the file";
+	
 	@Override
 	public void start(Stage primaryStage) {
+		primaryStage.setTitle(THE_TITLE);
+		
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			
+			URL theURL = super.getClass().getResource(VIEW_EVENT_PANE_FXML);
+			
+			AnchorPane thePane = FXMLLoader.load(theURL);
+			
+			Scene theScene = new Scene(thePane);
+
+			primaryStage.setScene(theScene);
+			
 			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
+		} catch (IOException theException) {
+			Alert message = new Alert(AlertType.ERROR);
+			message.setContentText(THE_IO_EXCEPTION_TEXT);
+			message.showAndWait();
+			
+		} catch (NullPointerException theException) {
+			Alert message = new Alert(AlertType.ERROR);
+			message.setContentText(THE_NUMBER_FORMAT_EXCEPTION_TEXT);
+			message.showAndWait();
 		}
+	
 	}
+	
+	/**
+	 * The main method for the weather project
+	 * 
+	 * @author Alex DeCesare
+	 * @version 16-July-2020
+	 * 
+	 * @param args the arguments
+	 */
 	
 	public static void main(String[] args) {
 		launch(args);
