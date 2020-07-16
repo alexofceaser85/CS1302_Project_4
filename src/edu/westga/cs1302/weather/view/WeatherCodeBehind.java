28e42650523fe113edebb237ef11f80e969fb1f9
alex decesare <adecesa1@my.westga.edu>
2020-07-16 19:36:51 -0400

    Added the view classes

---
+++ src/edu/westga/cs1302/weather/view/WeatherCodeBehind.java
@@ -0,0 +1,12 @@
+package edu.westga.cs1302.weather.view;
+
+/**
+ * The code behind class for the weather project
+ * 
+ * @author Alex DeCesare
+ * @version 16-July-2020
+ */
+
+public class WeatherCodeBehind {
+
+}
---
+++ src/edu/westga/cs1302/weather/view/WeatherViewModel.java
@@ -0,0 +1,12 @@
+package edu.westga.cs1302.weather.view;
+
+/**
+ * The view model class for the weather project
+ * 
+ * @author Alex DeCesare
+ * @version 16-July-2020
+ */
+
+public class WeatherViewModel {
+
+}
