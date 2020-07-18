d85ef71f28a3175cb8e448a7033d4e04d71d7ef8
alex decesare <adecesa1@my.westga.edu>
2020-07-18 18:48:55 -0400

    Added the code to open a file location

--- src/edu/westga/cs1302/weather/view/WeatherCodeBehind.java
+++ src/edu/westga/cs1302/weather/view/WeatherCodeBehind.java
@@ -1,5 +1,15 @@
 package edu.westga.cs1302.weather.view;
 
+import java.io.File;
+import java.nio.file.Paths;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
 /**
  * The code behind class for the weather project
  * 
@@ -8,5 +18,52 @@ package edu.westga.cs1302.weather.view;
  */
 
 public class WeatherCodeBehind {
+	
+	@FXML
+	private Pane thePane;
+
+    @FXML
+    private MenuItem buttonOpenFile;
+
+    @FXML
+    private MenuItem buttonExit;
+
+    @FXML
+    private MenuItem buttonAbout;
+    
+	private WeatherViewModel theWeatherViewModel;
+	
+	/**
+	 * The constructor for the code behind method
+	 * 
+	 * @precondition none
+	 * @postcondition none
+	 */
+	
+	public WeatherCodeBehind() {
+	
+		this.theWeatherViewModel = new WeatherViewModel();
+		
+	}
+	
+	/**
+	 * Chooses the file to parse and sets it to the projects file path
+	 * 
+	 * @precondition none
+	 * @postcondition none
+	 */
+	
+	@FXML
+	public void chooseFile() {
+		
+		Window stage = this.thePane.getScene().getWindow();
+		
+		FileChooser theFileChooser = new FileChooser();
+		String theCurrentFilePath = Paths.get(".").toAbsolutePath().normalize().toString();
+		
+		theFileChooser.setInitialDirectory(new File(theCurrentFilePath));
+		theFileChooser.showOpenDialog(stage);
+		
+	}
 
 }
