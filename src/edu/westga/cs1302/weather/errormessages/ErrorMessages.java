1745c0c5867754ad52b323edb1a327ba0591773f
alex decesare <adecesa1@my.westga.edu>
2020-07-18 14:33:17 -0400

    Added a try catch block for if the index is out of bounds

--- src/edu/westga/cs1302/weather/errormessages/ErrorMessages.java
+++ src/edu/westga/cs1302/weather/errormessages/ErrorMessages.java
@@ -24,4 +24,7 @@ public class ErrorMessages {
 	public static final String ALERT_THERE_WAS_AN_ERROR_PARSING_THE_DATE = "There was an error with the date";
 	public static final String ALERT_THERE_WAS_AN_ERROR_PARSING_THE_TEMPERATURE = "There was an error with parsing the temperature";
 	public static final String ALERT_THERE_WAS_AN_ERROR_READING_THE_FILE = "There was an error with reading the file";
+	public static final String ALERT_CANNOT_FIND_THE_FIRST_WEATHER_DAY = "Cannot find the first weather day";
+	public static final String ALERT_CANNOT_FIND_THE_MIDDLE_WEATHER_DAY = "Cannot find the middle weather day";
+	public static final String ALERT_CANNOT_FIND_THE_LAST_WEATHER_DAY = "Cannot find the last weather day";
 }
