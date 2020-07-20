f72c4bc85c26a073d204dffd7232f4e39271f43f
alex decesare <adecesa1@my.westga.edu>
2020-07-20 19:15:26 -0400

    Added the test code for the get first day method

---
+++ src/edu/westga/cs1302/weather/chart/testing/TestGetFirstDay.java
@@ -0,0 +1,58 @@
+package edu.westga.cs1302.weather.chart.testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs1302.weather.chart.ChartData;
+import edu.westga.cs1302.weather.model.WeatherForADay;
+import edu.westga.cs1302.weather.model.WeatherManager;
+
+class TestGetFirstDay {
+
+	@Test
+	public void shouldNotGetFirstDayeOfEmptyManager() {
+		
+		WeatherManager theManager = new WeatherManager();
+		ChartData theChartData = new ChartData(theManager);
+		
+		assertThrows(IndexOutOfBoundsException.class, () -> {
+			assertEquals(null, theChartData.getTheFirstDay());
+		});
+	}
+	
+	@Test
+	public void shouldGetFirstDayeOfManagerWithOneDay() {
+		
+		WeatherManager theManager = new WeatherManager();
+		
+		WeatherForADay theFirstWeather = new WeatherForADay("USC00091640", "CARROLLTON GA US", "2020-08-01", 92.5, 68.5);
+		
+		theManager.addWeatherDay(theFirstWeather);
+		
+		ChartData theChartData = new ChartData(theManager);
+		
+		assertEquals("2020-08-01", theChartData.getTheFirstDay());
+		
+	}
+	
+	@Test
+	public void shouldGetFirstDayeOfManagerWithMultipleDays() {
+		
+		WeatherManager theManager = new WeatherManager();
+		
+		WeatherForADay theFirstWeather = new WeatherForADay("USC00091640", "CARROLLTON GA US", "2020-08-01", 92, 68);
+		WeatherForADay theSecondWeather = new WeatherForADay("USC00091640", "CARROLLTON GA US", "2020-08-02", 91, 67);
+		WeatherForADay theThirdWeather = new WeatherForADay("USC00091640", "CARROLLTON GA US", "2020-08-03", 90, 66);
+		
+		theManager.addWeatherDay(theFirstWeather);
+		theManager.addWeatherDay(theSecondWeather);
+		theManager.addWeatherDay(theThirdWeather);
+		
+		ChartData theChartData = new ChartData(theManager);
+		
+		assertEquals("2020-08-01", theChartData.getTheFirstDay());
+		
+	}
+
+}
