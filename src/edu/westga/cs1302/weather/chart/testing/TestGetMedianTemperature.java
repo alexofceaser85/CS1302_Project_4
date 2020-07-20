9e4431bc27b88f7a7a6ee94161b5156eea8568c9
alex decesare <adecesa1@my.westga.edu>
2020-07-20 17:53:39 -0400

    Added the test code for the median temperature

---
+++ src/edu/westga/cs1302/weather/chart/testing/TestGetMedianTemperature.java
@@ -0,0 +1,58 @@
+package edu.westga.cs1302.weather.chart.testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs1302.weather.chart.ChartData;
+import edu.westga.cs1302.weather.model.WeatherForADay;
+import edu.westga.cs1302.weather.model.WeatherManager;
+
+class TestGetMedianTemperature {
+
+	@Test
+	public void shouldNotGetMedianTemperatureOfEmptyManager() {
+		
+		WeatherManager theManager = new WeatherManager();
+		ChartData theChartData = new ChartData(theManager);
+		
+		assertThrows(NullPointerException.class, () -> {
+			assertEquals(null, theChartData.getTheMedianTemperature());
+		});
+	}
+	
+	@Test
+	public void shouldGetMedianTemperatureOfManagerWithOneDay() {
+		
+		WeatherManager theManager = new WeatherManager();
+		
+		WeatherForADay theFirstWeather = new WeatherForADay("USC00091640", "CARROLLTON GA US", "2020-08-01", 92.5, 68.5);
+		
+		theManager.addWeatherDay(theFirstWeather);
+		
+		ChartData theChartData = new ChartData(theManager);
+		
+		assertEquals(80.5, theChartData.getTheMedianTemperature());
+		
+	}
+	
+	@Test
+	public void shouldGetMedianTemperatureOfManagerWithMultipleDays() {
+		
+		WeatherManager theManager = new WeatherManager();
+		
+		WeatherForADay theFirstWeather = new WeatherForADay("USC00091640", "CARROLLTON GA US", "2020-08-01", 92, 68);
+		WeatherForADay theSecondWeather = new WeatherForADay("USC00091640", "CARROLLTON GA US", "2020-08-02", 91, 67);
+		WeatherForADay theThirdWeather = new WeatherForADay("USC00091640", "CARROLLTON GA US", "2020-08-03", 90, 66);
+		
+		theManager.addWeatherDay(theFirstWeather);
+		theManager.addWeatherDay(theSecondWeather);
+		theManager.addWeatherDay(theThirdWeather);
+		
+		ChartData theChartData = new ChartData(theManager);
+		
+		assertEquals(79, theChartData.getTheMedianTemperature());
+		
+	}
+
+}
