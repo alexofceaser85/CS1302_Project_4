90990d0bf58270b4fb701994075f5f0039aa89fa
alex decesare <adecesa1@my.westga.edu>
2020-07-21 23:03:50 -0400

    Fixed the checkstyle errors

--- src/edu/westga/cs1302/weather/io/SaveWeatherFile.java
+++ src/edu/westga/cs1302/weather/io/SaveWeatherFile.java
@@ -3,8 +3,6 @@ package edu.westga.cs1302.weather.io;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 import edu.westga.cs1302.weather.model.WeatherForADay;
 import edu.westga.cs1302.weather.model.WeatherManager;
